894df5df14c83b67dea5759c44eb9c769b7cfa0b
James Gate <james@team16.co.uk>
2020-05-30 13:00:04 +0100

    Added a move function

--- Arcadia.Test/boardtests.fs
+++ Arcadia.Test/boardtests.fs
@@ -5,10 +5,28 @@ open NUnit.Framework
 
 [<RequireQualifiedAccess; TestFixture>]
 module BoardTests =
-    
+
     [<Test>]
     let ``Test making an empty board`` () =
         let uut = Board.empty ()
-        Board.print uut
+
+        let result =
+            Ok uut
+            |> Result.bind (Board.move (9, 9, White))
+            |> Result.bind (Board.move (10, 10, Black))
+
+        match result with
+        | Ok board -> Board.print board
+        | Error message -> printfn "Error: %s" message
+
         ()
 
+    [<Test>]
+    let ``Test making a bad move`` () =
+        let board = Board.empty () |> Board.move (0, 0, White)
+
+        let result = board |> Result.bind (Board.move (0, 0, Black))
+
+        match result with
+        | Ok _ -> Assert.Fail()
+        | Error message -> printfn "%s" message
--- Arcadia/board.fs
+++ Arcadia/board.fs
@@ -1,5 +1,7 @@
 namespace Arcadia
 
+type Board = Pebble option list list
+
 /// Functions for interacting with a Pente board
 [<RequireQualifiedAccess>]
 module Board =
@@ -20,5 +22,18 @@ module Board =
                             |> printf "%s"
                         )
                     printfn "|"
-                    
                 )
+ 
+    /// Make a move on a board.
+    /// TODO support taking
+    /// TODO look for P | ~P | ~P | P sequences on each axis and diagonal; remove the ~Ps.
+    let move (colIndex, rowIndex, pebble) board =
+        let row = List.item colIndex board
+        let currentPebble = List.item rowIndex row
+        
+        match currentPebble with
+        | Some currentPebble -> Error <| sprintf "(%d,%d) already occupied by %A" colIndex rowIndex currentPebble
+        | None ->
+            let newRow = (List.append (row |> List.take rowIndex) ((Some pebble) :: (List.skip (rowIndex + 1) row)))
+            let newBoard = (List.append (board |> List.take colIndex) (newRow :: (List.skip (colIndex + 1) board)))
+            Ok newBoard
